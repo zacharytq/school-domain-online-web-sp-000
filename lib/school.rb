7572f2c03cd00eba2adadebe99e18d53fbe850be
chic-page-6502 <zacharytq@gmail.com>
2019-08-09 23:50:18 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
-# code here!
+class School
+  def initialize(name)
+    @school = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    if @roster.has_key?(grade)
+      @roster[grade] << name
+    else
+      @roster[grade] = []
+      @roster[grade] << name
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+end
